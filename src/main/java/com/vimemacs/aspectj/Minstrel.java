bcfbb3b577d8df93b9925c029c5acabd7a8b88a3
VimIcewind <VimIcewind@gmail.com>
2017-05-18 17:44:50 +0800

    Aop demo

---
+++ src/main/java/com/vimemacs/aspectj/Minstrel.java
@@ -0,0 +1,14 @@
+package com.vimemacs.aspectj;
+
+/**
+ * Created by HWD on 2016/5/11.
+ */
+public class Minstrel {
+    public void singBeforeQuest() {
+        System.out.println("Fa la la; The knight is so brave!");
+    }
+
+    public void singAfterQuest() {
+        System.out.println("Tee hee he; The brave knight did embark on a quest!");
+    }
+}
