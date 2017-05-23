70ecd0a48d7298ea34f567e2489f214c034dda3f
VimIcewind <VimIcewind@gmail.com>
2017-05-23 17:14:49 +0800

    Create Bean with prototype

---
+++ src/main/java/com/vimemacs/springidol/Ticket.java
@@ -0,0 +1,7 @@
+package com.vimemacs.springidol;
+
+/**
+ * Created by HWD on 2017/5/23.
+ */
+public class Ticket {
+}
---
+++ src/main/java/com/vimemacs/springidol/TicketMain.java
@@ -0,0 +1,24 @@
+package com.vimemacs.springidol;
+
+import org.omg.CORBA.portable.ApplicationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by HWD on 2017/5/23.
+ */
+public class TicketMain {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-idol.xml");
+        Ticket ticket = (Ticket) ctx.getBean("ticket");
+        System.out.println(ticket);
+        Ticket ticket1 = (Ticket) ctx.getBean("ticket");
+        System.out.println(ticket1);
+
+        ApplicationContext ctx1 = new ClassPathXmlApplicationContext("spring/spring-idol.xml");
+        Ticket ticket2 = (Ticket) ctx1.getBean("ticket1");
+        System.out.println(ticket2);
+        Ticket ticket3 = (Ticket) ctx1.getBean("ticket1");
+        System.out.println(ticket3);
+    }
+}
