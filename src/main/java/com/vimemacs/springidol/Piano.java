e249c5f290858fdf5dee5a0bc48c089ea26129de
VimIcewind <VimIcewind@gmail.com>
2017-05-24 14:13:04 +0800

    Change saxophone to piano

---
+++ src/main/java/com/vimemacs/springidol/Piano.java
@@ -0,0 +1,13 @@
+package com.vimemacs.springidol;
+
+/**
+ * Created by HWD on 2017/5/24.
+ */
+public class Piano implements Instrument {
+    public Piano() {
+    }
+
+    public void play() {
+        System.out.println("PLINK PLINK PLINK");
+    }
+}
