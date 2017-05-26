145d242fbe1a0752eb91fa3a9008fa9193b062c3
VimIcewind <VimIcewind@gmail.com>
2017-05-26 19:00:01 +0800

    Injecting with Spring's Jave-based configuration

---
+++ src/main/java/com/vimemacs/springidol/SpringIdolConfig.java
@@ -0,0 +1,37 @@
+package com.vimemacs.springidol;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by HWD on 2017/5/26.
+ */
+@Configuration
+public class SpringIdolConfig {
+    @Bean
+    public Performer duke() {
+        return new Juggler();
+    }
+
+    @Bean
+    public Performer duke15() {
+        return new Juggler(15);
+    }
+
+    @Bean
+    public Performer kenny() {
+        Instrumentalist kenny = new Instrumentalist();
+        kenny.setSong("Jingle Bells");
+        return kenny;
+    }
+
+    @Bean
+    private Poem sonnet29() {
+        return new Sonnet29();
+    }
+
+    @Bean
+    public Performer poeticDuke() {
+        return new PoeticJuggler(sonnet29());
+    }
+}
