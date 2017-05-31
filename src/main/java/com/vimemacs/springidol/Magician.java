0562232c52257771ad962f5a062c4763d426cfb9
VimIcewind <VimIcewind@gmail.com>
2017-05-31 17:59:22 +0800

    Pass argument to annotated advice

--- src/main/java/com/vimemacs/springidol/Magician.java
+++ src/main/java/com/vimemacs/springidol/Magician.java
@@ -1,14 +1,24 @@
 package com.vimemacs.springidol;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by HWD on 2017/5/31.
  */
 @Component
+@Aspect
 public class Magician implements MindReader {
     private String thoughts;
 
+    @Pointcut("execution(* com.vimemacs.springidol.Thinker.thinkOfSomething(String)) && args(thoughts)")
+    public void thinking(String thoughts) {
+
+    }
+
+    @Before("thinking(thoughts)")
     public void interceptThoughts(String thoughts) {
         System.out.println("Intercepting volunteer's thoughts");
         this.thoughts = thoughts;
