a34017ddd51fcbe5151591112c1d7231ec425a8b
VimIcewind <VimIcewind@gmail.com>
2017-06-15 15:22:48 +0800

    Spring Hibernate

---
+++ src/main/java/com/vimemacs/spitter/persistence/HibernateSpitterDao.java
@@ -0,0 +1,47 @@
+package com.vimemacs.spitter.persistence;
+
+import com.vimemacs.spitter.domain.Spitter;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by HWD on 2017/6/15.
+ */
+@Repository
+public class HibernateSpitterDao implements SpitterDao {
+    private SessionFactory sessionFactory;
+
+    // 构造DAO
+    @Autowired
+    public HibernateSpitterDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    // 从 SessionFactory 中获取当前的 Session
+    private Session currentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    // 使用当前 Session
+    public void addSpitter(Spitter spitter) {
+        currentSession().save(spitter);
+    }
+
+    public Spitter getSpitterById(long id) {
+        return (Spitter) currentSession().get(Spitter.class, id);
+    }
+
+    public void saveSpitter(Spitter spitter) {
+        currentSession().update(spitter);
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+}
---
+++ src/test/java/com/vimemacs/spitter/jdbc/HibernateSpitterDaoTest.java
@@ -0,0 +1,20 @@
+package com.vimemacs.spitter.jdbc;
+
+import com.vimemacs.spitter.domain.Spitter;
+import com.vimemacs.spitter.persistence.HibernateSpitterDao;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by HWD on 2017/6/15.
+ */
+public class HibernateSpitterDaoTest {
+//    @Test
+//    public void addSpitterTest() {
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-hibernate.xml");
+//        Spitter spitter = (Spitter) ctx.getBean("spitter");
+//        HibernateSpitterDao hibernateSpitterDao = (HibernateSpitterDao) ctx.getBean("hibernateSpitterDao");
+//        hibernateSpitterDao.addSpitter(spitter);
+//    }
+}
